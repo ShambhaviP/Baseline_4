643215794f3e187cdef2a046b785b77a95de5364
ShambhaviP <shambhap@thoughtworks.com>
2015-09-10 11:36:37 +0530

    added a spec for validating the basic sales tax applicable on any item other than food or medical product or book

--- test/com/tw/baseline4/TestBasicSalesTax.java
+++ test/com/tw/baseline4/TestBasicSalesTax.java
@@ -13,4 +13,13 @@ public class TestBasicSalesTax {
 
         assertEquals(0.0, basicSalesTax.basicSalesTaxCalculator(), 0.0001);
     }
+
+    @Test
+    public void shouldBeTenPercentOfThePriceForAnItemOtherThanABookOrAMedicalProductOrAFoodStuff() {
+        Item item = new Item("Music CD", 3.99, 1);
+        BasicSalesTax basicSalesTax = new BasicSalesTax(item);
+
+        assertEquals(0.399, basicSalesTax.basicSalesTaxCalculator(), 0.0001);
+    }
+
 }
